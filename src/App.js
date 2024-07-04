72d3ac1a43120f6dadd6749c5acfcb006041fa30
satwik78901 <satwikreddy588@gmail.com>
2024-07-04 08:52:21 +0530

    delted header and fotter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 import ListEmployeeComponent from './components/ListEmployeeComponent';
-import HeaderComponent from './components/HeaderComponent';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AddEmployee from './components/AddEmployee';
 import LoginUser from './components/LoginUser';
+import Home from './components/Home';
 function App() {
   return (
     <div className="App">
       <Router>
-        <HeaderComponent />
         <Routes>
-          <Route exact path="/" Component={LoginUser}></Route>
+          <Route exact path="/" Component={Home}></Route>
+          <Route path="/login" Component={LoginUser}></Route>
           <Route path="/employees" Component={ListEmployeeComponent}></Route>
           <Route path="/add-employee" Component={AddEmployee}></Route>
           <Route path="/edit-employee/:id" Component={AddEmployee}></Route>
