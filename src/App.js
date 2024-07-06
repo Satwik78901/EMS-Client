e4c0607fd0618f0d7fe60f84a4de872f60305d61
satwik78901 <satwikreddy588@gmail.com>
2024-07-06 10:10:41 +0530

    Features Route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AddEmployee from './components/AddEmployee';
 import LoginUser from './components/LoginUser';
 import Home from './components/Home';
+import Features from './components/Features';
 function App() {
   return (
     <div className="App">
@@ -14,6 +15,7 @@ function App() {
           <Route path="/employees" Component={ListEmployeeComponent}></Route>
           <Route path="/add-employee" Component={AddEmployee}></Route>
           <Route path="/edit-employee/:id" Component={AddEmployee}></Route>
+          <Route path="/features" Component={Features}></Route>
         </Routes>
       </Router>
     </div>
