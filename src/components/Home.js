4444880ca6ec714d3880a17267a8ec47d9553e37
satwik78901 <satwikreddy588@gmail.com>
2024-07-08 09:07:59 +0530

    Added Image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../Home.css';
+import myImage from '../images/Employee.jpg';
 
 const Home = () => {
   return (
@@ -16,26 +17,28 @@ const Home = () => {
           <button className="button-styles">Contact</button>
         </Link>
       </div>
-      <div className="position">
-        <div className="contentWrap">
-          <div className="containerWrap">
-            <h1>Welcome to the Employee Management System (EMS)</h1>
-            <h2>Your Gateway to Efficient Employee Management</h2>
-            <p>
-              Our Employee Management System (EMS) is a powerful tool designed
-              to help administrators efficiently manage employee records and
-              attendance. With a user-friendly interface and comprehensive
-              capabilities, EMS simplifies your administrative tasks and
-              enhances productivity.
-            </p>
-            <p>
-              Ready to experience the efficiency and ease of our EMS? Click on
-              the <b>Login</b> button to access your dashboard. If you are new
-              to EMS and want to learn more, explore our <b>Features</b> page to
-              discover what we offer. For any inquiries, visit our{' '}
-              <b>Contact</b> page and get in touch with us.
-            </p>
+      <div className="contentWrap">
+        <img src={myImage} alt="Employee" className="image-adjust" />
+        <div className="text-position">
+          <div className="content">
+            <h2>Employee Management</h2>
           </div>
+
+          <h2>Your Gateway to Efficient Employee Management</h2>
+          <p>
+            Our Employee Management System (EMS) is a powerful tool designed to
+            help administrators efficiently manage employee records and
+            attendance. With a user-friendly interface and comprehensive
+            capabilities, EMS simplifies your administrative tasks and enhances
+            productivity.
+          </p>
+          <p>
+            Ready to experience the efficiency and ease of our EMS? Click on the{' '}
+            <b>Login</b> button to access your dashboard. If you are new to EMS
+            and want to learn more, explore our <b>Features</b> page to discover
+            what we offer. For any inquiries, visit our <b>Contact</b> page and
+            get in touch with us.
+          </p>
         </div>
       </div>
     </div>
