52febe822dacd231cab15ae5d5c44e715c824d7a
satwik78901 <satwikreddy588@gmail.com>
2024-07-10 08:41:37 +0530

    Removed bootstap css

--- src/components/ListEmployeeComponent.js
+++ src/components/ListEmployeeComponent.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import EmployeeService from '../services/EmployeeService';
 import { Link } from 'react-router-dom';
+import '../ListEmployee.css';
 const ListEmployeeComponent = () => {
   const [employees, setEmployees] = useState([]);
   const displayEmployee = () => {
@@ -26,39 +27,38 @@ const ListEmployeeComponent = () => {
       });
   };
   return (
-    <div className="container">
-      <h2 className="text-center"> List Employees</h2>
-      <Link to="/add-employee" className="btn btn-primary mb-2">
+    <div className="containerTable">
+      <h2 className="text-center">List Employees</h2>
+      <Link to="/add-employee" className="btn btn-add-employee">
         Add Employee
       </Link>
-      <table className="table table-bordered table-striped">
+      <table className="employee-table">
         <thead>
-          <th>Employee Id</th>
-          <th>Employee First Name</th>
-          <th>Employee Last Name</th>
-          <th>Employee Email Id</th>
-          <th>Actions</th>
+          <tr>
+            <th>Employee Id</th>
+            <th>Employee First Name</th>
+            <th>Employee Last Name</th>
+            <th>Employee Email Id</th>
+            <th>Actions</th>
+          </tr>
         </thead>
         <tbody>
           {employees.map((employee) => (
             <tr key={employee.id}>
               <td>{employee.id}</td>
-              <td>{employee.firstName} </td>
-              <td>{employee.lastName} </td>
-              <td>{employee.email} </td>
+              <td>{employee.firstName}</td>
+              <td>{employee.lastName}</td>
+              <td>{employee.email}</td>
               <td>
                 <Link
                   to={`/edit-employee/${employee.id}`}
-                  className="btn btn-info"
+                  className="btn btn-update"
                 >
                   Update
                 </Link>
                 <button
-                  className="btn btn-danger"
-                  onClick={() => {
-                    deleteEmployee(employee.id);
-                  }}
-                  style={{ marginLeft: '10px' }}
+                  className="btn btn-delete"
+                  onClick={() => deleteEmployee(employee.id)}
                 >
                   Delete
                 </button>
